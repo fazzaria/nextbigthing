ad321fb86685228f8938e39163670353d81fc74c
Andrew Fazzari <fazzari_a@westat.com>
2017-01-27 11:40:05 -0500

    to do: rewrite api calls

--- app/routes.js
+++ app/routes.js
@@ -5,12 +5,16 @@ module.exports = function() {
 	var router = require('express').Router();
 
 	router.use(function(req, res, next) {
+		console.log(req.body);
+		//authenticate user id
+		//rewrite all api calls/data requests in chatsockets
 		next();
 	});
 
-	router.get('/', function(req, res) {
+	/*router.get('/a', function(req, res) {
+		console.log('api');
 		res.json({message: "api is working."});
-	});
+	});*/
 
 	require('./api/msgApi')(router);
 	require('./api/userApi')(router);
